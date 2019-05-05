206123138027be3ee96b1cc5b72ebc62c8fdfb3e
Muhamad Azmy <muhamad.azmy@codescalers.com>
2019-05-05 10:15:44 +0200

    Start services from config files

---
+++ src/app.rs
@@ -0,0 +1,46 @@
+use crate::manager;
+use crate::settings;
+
+use failure::Error;
+use future::lazy;
+use tokio::prelude::*;
+
+type Result<T> = std::result::Result<T, Error>;
+
+/// init start init command, immediately monitor all services
+/// that are defined under the config directory
+pub fn init(config: &str) -> Result<()> {
+    // load config
+    let configs = settings::load_dir(config, |file, err| {
+        println!(
+            "encountered err {} while loading file {:?}. skipping!",
+            err, file
+        );
+        settings::Walk::Continue
+    })?;
+
+    // start the tokio runtime, start the process manager
+    // and monitor all configured services
+    // TODO:
+    // We need to start the unix socket server that will
+    // receive and handle user management commands (start, stop, status, etc...)
+    tokio::run(lazy(|| {
+        // creating a new instance from the process manager
+        let manager = manager::Manager::new();
+
+        // running the manager returns a handle that we can
+        // use to actually control the process manager
+        // currently the handle only exposes one method
+        // `monitor` which spawns a new task on the process
+        // manager given the configuration
+        let handle = manager.run();
+
+        for (name, config) in configs.into_iter() {
+            handle.monitor(name, config);
+        }
+
+        Ok(())
+    }));
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,49 @@
-use future::lazy;
-use tokio::prelude::*;
+use clap::{App, Arg, SubCommand};
 
+mod app;
 #[allow(dead_code)]
 mod manager;
 #[allow(dead_code)]
 mod settings;
 
-extern crate futures;
 #[macro_use]
 extern crate failure;
 
 fn main() {
-    tokio::run(lazy(|| {
-        // creating a new instance from the process manager
-        let manager = manager::Manager::new();
+    let matches = App::new("zinit")
+        .author("Muhamad Azmy, https://github.com/muhamadazmy")
+        .version("0.1")
+        .about("A runit replacement")
+        .subcommand(
+            SubCommand::with_name("init")
+                .arg(
+                    Arg::with_name("config")
+                        .value_name("DIR")
+                        .short("c")
+                        .long("config")
+                        .help("service configurations directory")
+                        .default_value("/etc/zinit/"),
+                )
+                .about("run in init mode, start and maintain configured services"),
+        )
+        .get_matches();
 
-        // running the manager returns a handle that we can
-        // use to actually control the process manager
-        // currently the handle only exposes one method
-        // `monitor` which spawns a new task on the process
-        // manager given the configuration
-        let handle = manager.run();
+    let result = match matches.subcommand() {
+        ("init", Some(matches)) => app::init(matches.value_of("config").unwrap()),
+        _ => {
+            // TODO: replace with a call to default command
+            // this now can be `init` but may be a `status` command
+            // would be more appropriate
+            println!("try help");
+            return;
+        }
+    };
 
-        // example long running service with test
-        // handle.monitor(
-        //     "test".to_string(),
-        //     settings::Service {
-        //         exec: String::from("bash -c 'sleep 0.5s; redis-server'"),
-        //         test: String::from("redis-cli ping"),
-        //         one_shot: false,
-        //         after: vec![],
-        //     },
-        // );
-
-        // example service with dependencies
-        // the 3 together will print `hello world\n`
-
-        handle.monitor(
-            "third".to_string(),
-            settings::Service {
-                exec: String::from("echo"),
-                test: String::from(""),
-                one_shot: true,
-                after: vec![String::from("first"), String::from("second")],
-            },
-        );
-
-        handle.monitor(
-            "second".to_string(),
-            settings::Service {
-                exec: String::from("echo -n world"),
-                test: String::from(""),
-                one_shot: true,
-                after: vec![String::from("first")],
-            },
-        );
-
-        handle.monitor(
-            "first".to_string(),
-            settings::Service {
-                exec: String::from("echo -n 'hello '"),
-                test: String::from(""),
-                one_shot: true,
-                after: vec![],
-            },
-        );
-
-        Ok(())
-    }));
+    match result {
+        Ok(_) => return,
+        Err(e) => {
+            println!("{}", e);
+            std::process::exit(1);
+        }
+    }
 }
