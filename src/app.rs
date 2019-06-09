60db5018e63d66bb462e7aa7c55955962959c735
Muhamad Azmy <muhamad.azmy+git@gmail.com>
2019-06-09 11:07:56 +0200

    Finalize cmdline interface

--- src/app.rs
+++ src/app.rs
@@ -4,6 +4,7 @@ use crate::settings;
 
 use failure::Error;
 use future::lazy;
+use std::collections::HashMap;
 use std::io::{self, BufRead};
 use std::os::unix::net;
 use std::path;
@@ -13,8 +14,12 @@ type Result<T> = std::result::Result<T, Error>;
 
 /// init start init command, immediately monitor all services
 /// that are defined under the config directory
-pub fn init(config: &str) -> Result<()> {
+pub fn init(config: &str, debug: bool) -> Result<()> {
     // load config
+    if !debug && std::process::id() != 1 {
+        bail!("can only run as pid 1");
+    }
+
     std::env::set_current_dir(config)?;
 
     let configs = settings::load_dir(".", |file, err| {
@@ -57,15 +62,133 @@ pub fn init(config: &str) -> Result<()> {
     Ok(())
 }
 
-pub fn list() -> Result<()> {
-    let p = path::Path::new("/var/run").join(api::SOCKET_NAME);
-    let mut con = net::UnixStream::connect(p)?;
-    con.write_all(b"list\n")?;
+/// Status is response status from server
+/// Only OK, and ERROR are available now
+enum Status {
+    Ok,
+    Error,
+}
 
-    let mut reader = io::BufReader::new(con);
-    for line in reader.lines() {
-        println!("line {:?}", line);
+impl std::str::FromStr for Status {
+    type Err = Error;
+    fn from_str(s: &str) -> Result<Status> {
+        match s.trim().to_lowercase().as_ref() {
+            "ok" => Ok(Status::Ok),
+            "error" => Ok(Status::Error),
+            _ => bail!("unknown status"),
+        }
     }
+}
 
+/// Line protocol implementation
+trait APIProtocol: io::Read + io::Write {
+    fn request(&mut self, cmd: &str) -> Result<String>;
+}
+
+impl APIProtocol for net::UnixStream {
+    fn request(&mut self, cmd: &str) -> Result<String> {
+        let mut headers = HashMap::new();
+        self.write_all(cmd.as_bytes())?;
+        self.write_all(b"\n")?;
+        //let lines = read.lines();
+        let mut read = io::BufReader::new(self);
+        loop {
+            let mut line = String::new();
+            read.read_line(&mut line)?;
+            let line = line.trim();
+            if line == "" {
+                //end of headers section
+                break;
+            }
+
+            let parts: Vec<&str> = line.splitn(2, ":").collect();
+            if parts.len() != 2 {
+                bail!("invalid header syntax");
+            }
+
+            headers.insert(parts[0].trim().to_string(), parts[1].trim().to_string());
+        }
+
+        let count = headers
+            .get("lines")
+            .ok_or(format_err!("lines header not provided"))
+            .map(|v| v.parse::<usize>())??;
+
+        let status = headers
+            .get("status")
+            .ok_or(format_err!("status header not provided"))
+            .map(|v| v.parse::<Status>())??;
+
+        let mut buffer = String::new();
+        for _ in 0..count {
+            read.read_line(&mut buffer)?;
+        }
+        buffer.pop(); //remove last new line char
+        match status {
+            Status::Ok => Ok(buffer),
+            Status::Error => bail!("{}", buffer),
+        }
+    }
+}
+
+fn connect() -> Result<net::UnixStream> {
+    let p = path::Path::new("/var/run").join(api::SOCKET_NAME);
+
+    Ok(net::UnixStream::connect(p)?)
+}
+
+/// list command
+pub fn list() -> Result<()> {
+    let mut con = connect()?;
+    let result = con.request("list")?;
+    println!("{}", result);
+    Ok(())
+}
+
+/// status command
+pub fn status(name: &str) -> Result<()> {
+    let mut con = connect()?;
+    let result = con.request(&format!("status {}", name))?;
+    println!("{}", result);
+    Ok(())
+}
+
+/// stop command
+pub fn stop(name: &str) -> Result<()> {
+    let mut con = connect()?;
+    let result = con.request(&format!("stop {}", name))?;
+    println!("{}", result);
+    Ok(())
+}
+
+/// start command
+pub fn start(name: &str) -> Result<()> {
+    let mut con = connect()?;
+    let result = con.request(&format!("start {}", name))?;
+    println!("{}", result);
+    Ok(())
+}
+
+/// forget command
+pub fn forget(name: &str) -> Result<()> {
+    let mut con = connect()?;
+    let result = con.request(&format!("forget {}", name))?;
+    println!("{}", result);
+    Ok(())
+}
+
+/// forget command
+pub fn monitor(name: &str) -> Result<()> {
+    let mut con = connect()?;
+    let result = con.request(&format!("monitor {}", name))?;
+    println!("{}", result);
+    Ok(())
+}
+
+/// kill command
+pub fn kill(name: &str, signal: &str) -> Result<()> {
+    let mut con = connect()?;
+    let result = con.request(&format!("kill {} {}", name, signal))?;
+    println!("{}", result);
     Ok(())
 }
