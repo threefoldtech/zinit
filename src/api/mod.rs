threefoldtech/zinit

commit 3856a4a719c74c94193b55f9ab7a029446ddc111

Add path of file to error logs in init (#26)
* add path of file to error log, fixes #14

* resolves pr commets

* resolves pr comment

src/api/mod.rs
@@ -3,9 +3,12 @@ use failure::Error;
 use future;
 use nix::sys::signal;
 use shlex;
+use snafu::{ResultExt, Snafu}; // 0.2.3
+use std::fs;
+use std::io;
+use std::path::{Path, PathBuf};
 use std::str::FromStr;
 use std::sync::Arc;
-use std::{fs, path};
 use tokio::codec::{Framed, LinesCodec};
 use tokio::net::{UnixListener, UnixStream};
 use tokio::prelude::*;
@@ -17,6 +20,13 @@ pub const SOCKET_NAME: &str = "zinit.sock";
 pub const RINGLOG_NAME: &str = "log.sock";
 
 type Result<T> = std::result::Result<T, Error>;
+type PathResult<T> = std::result::Result<T, ProgramError>;
+
+#[derive(Debug, Snafu)]
+pub enum ProgramError {
+    #[snafu(display("{}: {}", path.display(), source))]
+    Metadata { source: io::Error, path: PathBuf },
+}
 
 fn list(handle: Handle) -> Result<Option<String>> {
     let mut result = String::new();
@@ -175,13 +185,14 @@ fn process(handle: Handle, socket: UnixStream) {
     tokio::spawn(future);
 }
 
-pub fn run(handle: Handle) -> Result<()> {
-    let p = path::Path::new("/var/run");
-    fs::create_dir_all(p)?;
-    let p = p.join(SOCKET_NAME);
-    let _ = fs::remove_file(&p);
+pub fn run(handle: Handle) -> PathResult<()> {
+    let path = Path::new("/var/run");
+    fs::create_dir_all(path).context(Metadata { path })?;
+    let path = path.join(RINGLOG_NAME);
+    let _ = fs::remove_file(&path);
 
-    let server = UnixListener::bind(p)?
+    let server = UnixListener::bind(&path)
+        .context(Metadata { path })?
         .incoming()
         .map_err(|e| error!("accept err: {}", e))
         .for_each(move |socket| {
@@ -194,13 +205,14 @@ pub fn run(handle: Handle) -> Result<()> {
     Ok(())
 }
 
-pub fn logd(log: Arc<RingLog>) -> Result<()> {
-    let p = path::Path::new("/var/run");
-    fs::create_dir_all(p)?;
-    let p = p.join(RINGLOG_NAME);
-    let _ = fs::remove_file(&p);
+pub fn logd(log: Arc<RingLog>) -> PathResult<()> {
+    let path = Path::new("/var/run");
+    fs::create_dir_all(path).context(Metadata { path })?;
+    let path = path.join(RINGLOG_NAME);
+    let _ = fs::remove_file(&path);
 
-    let server = UnixListener::bind(p)?
+    let server = UnixListener::bind(&path)
+        .context(Metadata { path })?
         .incoming()
         .map_err(|e| error!("accept err: {}", e))
         .for_each(move |socket| {

Cargo.toml
@@ -18,3 +18,4 @@ bitflags = "1.0.4"
 log = "0.4"
 fern = "0.5"
 rand = "0.7.2"
+snafu = "0.6.2"

commit 34542dd4ca524d0e51ae2f48bf120f53ce30c29a

Fix regression bug
Fix issue introduced by
https://github.com/threefoldtech/zinit/commit/3856a4a719c74c94193b55f9ab7a029446ddc111#diff-3c1b0c147df4a347a97a4f35aeda4af0L181

src/api/mod.rs
@@ -188,7 +188,7 @@ fn process(handle: Handle, socket: UnixStream) {
 pub fn run(handle: Handle) -> PathResult<()> {
     let path = Path::new("/var/run");
     fs::create_dir_all(path).context(Metadata { path })?;
-    let path = path.join(RINGLOG_NAME);
+    let path = path.join(SOCKET_NAME);
     let _ = fs::remove_file(&path);
 
     let server = UnixListener::bind(&path)

src/app.rs
@@ -144,7 +144,10 @@ impl APIProtocol for net::UnixStream {}
 fn connect() -> Result<net::UnixStream> {
     let p = path::Path::new("/var/run").join(api::SOCKET_NAME);
 
-    Ok(net::UnixStream::connect(p)?)
+    match net::UnixStream::connect(&p) {
+        Ok(socket) => Ok(socket),
+        Err(err) => bail!("{:?}: {}", p, err),
+    }
 }
 
 /// list command