a5fab4a078b45587c944407dd3c9ea2a37145159
Muhamad Azmy <muhamad@incubaid.com>
2021-09-28 12:18:35 +0200

    Better iterator (#31)
    * Add iterator struct
    
    Signed-off-by: Lee Smet <lee.smet@hotmail.com>
    
    * handle lagging
    
    * use arc
    
    Co-authored-by: Lee Smet <lee.smet@hotmail.com>

--- src/manager/buffer.rs
+++ src/manager/buffer.rs
@@ -1,9 +1,10 @@
 use anyhow::Result;
 use std::sync::Arc;
 use tokio::sync::broadcast;
+use tokio::sync::broadcast::error::RecvError;
 use tokio::sync::{mpsc, Mutex};
 
-struct Buffer<T: Clone> {
+struct Buffer<T> {
     inner: Vec<T>,
     at: usize,
 }
@@ -33,20 +34,51 @@ impl<T: Clone> Buffer<T> {
 
         self.at = (self.at + 1) % self.cap();
     }
+}
 
-    pub fn iter(&self) -> Vec<T> {
-        let (a, b) = self.inner.split_at(self.at);
+impl<'a, T: 'a> IntoIterator for &'a Buffer<T> {
+    type IntoIter = BufferIter<'a, T>;
+    type Item = &'a T;
 
-        b.iter().cloned().chain(a.iter().cloned()).collect()
+    fn into_iter(self) -> Self::IntoIter {
+        let (second, first) = self.inner.split_at(self.at);
+
+        BufferIter {
+            first,
+            second,
+            index: 0,
+        }
     }
 }
 
-pub type Logs = mpsc::Receiver<String>;
+pub struct BufferIter<'a, T> {
+    first: &'a [T],
+    second: &'a [T],
+    index: usize,
+}
+
+impl<'a, T> Iterator for BufferIter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let index = self.index;
+        self.index += 1;
+        if index < self.first.len() {
+            Some(&self.first[index])
+        } else if index - self.first.len() < self.second.len() {
+            Some(&self.second[index - self.first.len()])
+        } else {
+            None
+        }
+    }
+}
+
+pub type Logs = mpsc::Receiver<Arc<String>>;
 
 #[derive(Clone)]
 pub struct Ring {
-    buffer: Arc<Mutex<Buffer<String>>>,
-    sender: broadcast::Sender<String>,
+    buffer: Arc<Mutex<Buffer<Arc<String>>>>,
+    sender: broadcast::Sender<Arc<String>>,
 }
 
 impl Ring {
@@ -59,23 +91,39 @@ impl Ring {
     }
 
     pub async fn push(&self, line: String) -> Result<()> {
-        self.buffer.lock().await.push(line.clone());
-        self.sender.send(line.clone())?;
+        let line = Arc::new(line.clone());
+        self.buffer.lock().await.push(Arc::clone(&line));
+        self.sender.send(line)?;
         Ok(())
     }
 
     pub async fn stream(&self) -> Result<Logs> {
-        let (tx, stream) = mpsc::channel::<String>(100);
+        let (tx, stream) = mpsc::channel::<Arc<String>>(100);
         let mut rx = self.sender.subscribe();
-        let buffer = self.buffer.lock().await.iter();
+        let buffer = self
+            .buffer
+            .lock()
+            .await
+            .into_iter()
+            .cloned()
+            .collect::<Vec<_>>();
         tokio::spawn(async move {
             for item in buffer {
-                let _ = tx.send(item).await;
+                let _ = tx.send(Arc::clone(&item)).await;
             }
 
-            while let Ok(line) = rx.recv().await {
-                if let Err(_) = tx.send(line).await {
+            loop {
+                let line = match rx.recv().await {
+                    Ok(line) => line,
+                    Err(RecvError::Closed) => break,
+                    Err(RecvError::Lagged(n)) => {
+                        Arc::new(format!("[-] zinit: {} lines dropped", n))
+                    }
+                };
+
+                if let Err(err) = tx.send(line).await {
                     // log here!
+                    error!("failed to send line to stream: {}", err);
                     break;
                 }
             }
