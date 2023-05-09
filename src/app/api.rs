506f1d071b91c9b53e5baa0373852111fed30768
mario <mariobassem12@gmail.com>
2023-05-09 15:53:52 +0300

    update connect error message to be more contextual

--- src/app/api.rs
+++ src/app/api.rs
@@ -242,9 +242,12 @@ impl Client {
     }
 
     async fn connect(&self) -> Result<UnixStream> {
-        Ok(UnixStream::connect(&self.socket).await.context(
-            "Failed connect to a running instance of zinit. make sure to run `zinit init`",
-        )?)
+        Ok(UnixStream::connect(&self.socket).await.with_context(|| {
+            format!(
+                "failed to connect to '{:?}'. is zinit listening on that socket?",
+                self.socket
+            )
+        })?)
     }
 
     async fn command(&self, c: &str) -> Result<Value> {
